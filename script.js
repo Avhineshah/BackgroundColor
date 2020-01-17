a9405e55a96c00fd10f3693ff45618a95afbaf39
UmmuMuhamad <amirah@kidocode.com>
2020-01-17 20:42:53 +0800

    Finishing My Third Project

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
-const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
-const hexBtn = document.querySelector('.hexBtn');
-const bodyBcg = document.querySelector('body');
-const hex = document.querySelector('.hex');
+const quotes = [
+{
+ name:'Thalapathy Vijay',
+ quote:'Kill Them With Success And Bury Them With A Smile'
+},
+{
+ name:'Rajinikanth',
+ quote: 'Crush your ego then you will find who you are'
+},
+{
+ name:'Dhanush',
+ quote:'A Smooth Life Has Never Made A Strong Person'
+},
+{
+ name:'HipHop Tamizha', 
+ quote:'Push yourself because No one else is going do it for you'
 
-hexBtn.addEventListener('click',getHex);
+},
+{
+ name:'Thala Ajith',
+ quote:'Love your haters because they are your biggest fans'
+} 
+]
 
-function getHex (){
-let hexCol = '#';
+const quoteBtn = document.querySelector('#quoteBtn');
+const quoteAuthor = document.querySelector('#quoteAuthor');
+const quote = document.querySelector('#quote');
 
-for(let i = 0; i<6;i++){
-let random = Math.floor(Math.random()*hexNumbers.length);
-console.log(random);
-hexCol +=hexNumbers[random];
-console.log(hexCol);
+quoteBtn.addEventListener('click',displayQuote);
 
+function displayQuote (){
 
+    let number = Math.floor(Math.random()*quotes.length);
 
+    console.log(number);
+
+    quoteAuthor.innerHTML = quotes[number].name;
+    quote.innerHTML = quotes[number].quote;
 
 }
-bodyBcg.style.backgroundColor = hexCol;
-hex.innerHTML = hexCol;
+
+//const simpleQuotes = [
+
+//{
+ //name:'author number 1',
+ //quote:'quote number 1'
+//},
+//{
+ //name:'author number 2',
+ //quote:'quote number 2'
+//}
 
 
 
 
 
-}
+
+
+
